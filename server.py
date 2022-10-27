7f272105d73b7b16052ba1a18dfbe92c557d07c4
nmtMike <108139735+nmtMike@users.noreply.github.com>
2022-10-27 12:21:12 +0700

    run python script using runpy module

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/ETL_Data')
 def ELT():
-    file_globals = runpy.run_path("VTA_pipline2.py")
+    runpy.run_path("VTA_pipline2.py")
     return render_template('ETL_data.html')
 
 if __name__ == '__main__':
